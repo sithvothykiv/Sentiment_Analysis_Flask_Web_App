9c5970ac4f77269f95c81752f4bf1c253dcf4cc0
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2024-03-03 09:23:41 +0700

    init commit

---
+++ AI_App/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, flash, request, url_for, redirect, session
+import numpy as np
+import re
+import os
+import tensorflow as tf
+from numpy import array
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+IMAGE_FOLDER = os.path.join('static', 'img_pool')
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+
+# model = load_model('sentiment_analysis.h5')
+# graph = tf.compat.v1.get_default_graph()
+model = tf.keras.models.load_model('sentiment_analysis.h5')
+
+
+# Initialize the variables
+sess = tf.compat.v1.keras.backend.get_session()
+sess.run(tf.compat.v1.global_variables_initializer())
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template("home.html")
+
+
+@app.route('/sentiment_analysis_prediction', methods=['POST', "GET"])
+def sent_analyse_prediction():
+    if request.method == 'POST':
+        text = request.form['text']
+        max_review_length = 500
+        word_to_id = imdb.get_word_index()
+        strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+        text = text.lower().replace("<br />", " ")
+        text = re.sub(strip_special_chars, "", text.lower())
+
+        words = text.split()  # split string into a list
+        x_test = [[word_to_id[word] if (word in word_to_id and word_to_id[word] <= 20000) else 0 for word in words]]
+        x_test = sequence.pad_sequences(x_test, maxlen=max_review_length)  # Should be same which you used for training data
+        vector = np.array([x_test.flatten()])
+
+        # with graph.as_default():
+        probability = model.predict(array([vector][0]))[0][0]
+        class1 = model.predict_classes(array([vector][0]))[0][0]
+
+        if class1 == 0:
+            sentiment = 'Negative'
+            img_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Sad_Emoji.png')
+        else:
+            sentiment = 'Positive'
+            img_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Smiling_Emoji.png')
+    return render_template('home.html', text=text, sentiment=sentiment, probability=probability, image=img_filename)
+
+
+if __name__ == "__main__":
+    app.run()
